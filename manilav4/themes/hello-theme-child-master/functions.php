a3dc3f9467129b349040328a2a35fc78b2a62dcb
Ruel Mendoza <ruel.r.mendoza@gmail.com>
2025-02-10 12:44:22 +0000

    Main update

--- manilav4/themes/hello-theme-child-master/functions.php
+++ manilav4/themes/hello-theme-child-master/functions.php
@@ -119,7 +119,7 @@ include_once('includes/team_performance.php');
 
 // DISABLE ELEMENTOR METADATA
 add_filter( "hello_elementor_description_meta_tag", "__return_false" );
-
+// REDIRECT USER AFTER LOGOUT
 add_action('wp_logout','auto_redirect_after_logout');
 
 function auto_redirect_after_logout(){
